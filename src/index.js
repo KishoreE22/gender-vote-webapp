d75c5faadb2294e88dae1ff82ff8009ed3ff4f9d
KishoreE22 <kishorepasala6@gmail.com>
2025-04-18 07:44:30 +0530

    Added code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GenderVoteApp from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GenderVoteApp />);
