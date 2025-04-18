355db147819c89c99f4f972e7588850081310d5b
KishoreE22 <kishorepasala6@gmail.com>
2025-04-18 07:52:46 +0530

    Added code

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GenderVoteApp from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GenderVoteApp />
+  </React.StrictMode>
+);
